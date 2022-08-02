e12013a79e45a9299c961ccbe86f09a7a3aff7b1
Victor <Victor@DESKTOP-SKOFSMV>
2022-08-02 06:58:46 -0300

    Add project files.

---
+++ Exercicio9/Aluno.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio9
+{
+    internal class Aluno
+    {
+        
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;                
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double NotaReprovado()
+        {
+            return 60 - NotaFinal();
+        }
+    }
+}
